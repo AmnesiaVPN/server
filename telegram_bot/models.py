f685b0024ae2bca0c58db695a316013d59208a5d
Eldos <eldos.baktybekov@gmail.com>
2022-11-15 10:32:29 +0600

    :card_file_box: Add field `is_subscribed` into `User` model

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
     uuid = models.UUIDField()
     server = models.ForeignKey(Server, on_delete=models.CASCADE)
     is_trial_period = models.BooleanField(default=True)
+    is_subscribed = models.BooleanField(default=True)
     registered_at = models.DateTimeField(auto_now_add=True)
     subscribed_at = models.DateTimeField(auto_now_add=True)
 
