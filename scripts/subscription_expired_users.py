5d7b2830b3395a6857f1838c10c2e40c3d1229a2
Eldos <eldos.baktybekov@gmail.com>
2022-11-15 10:33:01 +0600

    :sparkles: Add scripts that will work in background

---
+++ scripts/subscription_expired_users.py
@@ -0,0 +1,60 @@
+import collections
+import datetime
+import os
+from typing import Iterable, DefaultDict
+
+from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.db.models import Q
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vobla_vpn.settings')
+application = get_wsgi_application()
+
+from wireguard.exceptions import VPNServerError
+from telegram_bot.exceptions import TelegramAPIError
+from wireguard.models import Server
+from wireguard.services import vpn_server
+
+from telegram_bot.models import User
+from telegram_bot import telegram
+
+
+def group_users_by_server(users: Iterable[User]) -> DefaultDict[Server, list[User]]:
+    server_to_users = collections.defaultdict(list)
+    for user in users:
+        server_to_users[user.server].append(user)
+    return server_to_users
+
+
+def main():
+    now = datetime.datetime.utcnow()
+    subscription_period_timedelta = datetime.timedelta(days=settings.SUBSCRIPTION_DAYS)
+    expired_trial_period_users = (
+        User.objects
+        .select_related('server')
+        .filter(is_subscribed=True, subscribed_at__lte=now - subscription_period_timedelta)
+        .all()
+    )
+    users_to_be_updated = []
+    server_to_users = group_users_by_server(expired_trial_period_users)
+    for server, users in server_to_users.items():
+        with vpn_server.connected_client(server.url, server.password) as client:
+            for user in users:
+                user.is_subscribed = False
+                users_to_be_updated.append(user)
+
+                try:
+                    vpn_server.disable_user(client, user.uuid)
+                except VPNServerError:
+                    print(f'Unable to disable user')
+
+                try:
+                    telegram.send_message(user.telegram_id, 'Ваш ваша подписка закончилась')
+                except TelegramAPIError:
+                    print(f'Unable to send message to user {user.telegram_id}')
+
+    User.objects.bulk_update(users_to_be_updated, ('is_subscribed',))
+
+
+if __name__ == '__main__':
+    main()
