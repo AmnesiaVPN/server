c2eb212b866aeb7ef5276abf8edff5b2d7817a83
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 16:37:19 +0600

    :goal_net: Catch errors working with donationalerts API

--- donationalerts/schemas.py
+++ donationalerts/schemas.py
@@ -6,6 +6,3 @@ from pydantic import BaseModel
 class Donation(BaseModel):
     id: int
     message: str
-    amount: int
-    currency: str
-    created_at: datetime.datetime
--- donationalerts/services.py
+++ donationalerts/services.py
@@ -8,10 +8,11 @@ from donationalerts.schemas import Donation
 
 
 class DonationAlertsClient:
-    __slots__ = ('__access_token',)
+    __slots__ = ('__access_token', '__attempts_on_server_error_count')
 
-    def __init__(self, access_token: str):
+    def __init__(self, access_token: str, *, attempts_on_server_error_count: int = 10):
         self.__access_token = access_token
+        self.__attempts_on_server_error_count = attempts_on_server_error_count
 
     @property
     def headers(self) -> dict:
@@ -19,16 +20,25 @@ class DonationAlertsClient:
 
     def iter_all_donations(self, *, page: int = 1) -> Generator[list[Donation], None, None]:
         url = 'https://www.donationalerts.com/api/v1/alerts/donations'
+        server_errors_count = 0
         with httpx.Client(headers=self.headers) as client:
             while True:
                 params = {'page': page}
-                response = client.get(url, params=params)
+
+                try:
+                    response = client.get(url, params=params)
+                except httpx.HTTPError:
+                    server_errors_count += 1
+                    continue
+
+                if response.is_server_error and server_errors_count < self.__attempts_on_server_error_count:
+                    server_errors_count += 1
+                    continue
+
                 response_json = response.json()
                 if not response_json['data']:
                     break
                 yield parse_obj_as(list[Donation], response_json['data'])
-                if response_json['links']['next'] is None:
-                    break
                 page += 1
 
 
