93fab009c89ab3309e7dab66ecc39c24b88ab27c
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 14:33:37 +0600

    :card_file_box: Add payments history

---
+++ donationalerts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from telegram_bot.models import User
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    donation_id = models.BigIntegerField(unique=True, db_index=True)
