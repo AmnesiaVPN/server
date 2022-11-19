6c17cf1f2c020fa613380bd615c0f531211aa392
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 23:06:05 +0600

    :card_file_box: Add additional fields into Payment model

--- donationalerts/models.py
+++ donationalerts/models.py
@@ -6,3 +6,5 @@ from telegram_bot.models import User
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     donation_id = models.BigIntegerField(unique=True, db_index=True)
+    created_at = models.DateTimeField()
+    is_used = models.BooleanField(default=False)
