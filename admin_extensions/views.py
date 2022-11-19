48441c76f5bf29bbe3df243b456819638e466c51
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 12:16:19 +0600

    :sparkles: Add broadcasting functionality

--- admin_extensions/views.py
+++ admin_extensions/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from admin_extensions.forms import BroadcastForm
+from telegram_bot.tasks import start_broadcasting
 
 
 class BroadcastView(FormView):
@@ -11,5 +12,8 @@ class BroadcastView(FormView):
     success_url = reverse_lazy('broadcast')
 
     def form_valid(self, form):
+        text = form.cleaned_data['text']
+        results_notify_to = form.cleaned_data['results_notify_to']
+        start_broadcasting.delay(text, results_notify_to)
         messages.add_message(self.request, messages.SUCCESS, 'Рассылка создана')
         return super().form_valid(form)
