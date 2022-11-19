c7f60b94419b68a9eec79496f103a8eeecc5b74d
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 12:23:08 +0600

    :lipstick: Add placeholders

--- admin_extensions/forms.py
+++ admin_extensions/forms.py
@@ -3,5 +3,14 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BroadcastForm(forms.Form):
-    text = forms.CharField(max_length=4000, min_length=5, widget=forms.Textarea(), label=_('Broadcast message'))
-    results_notify_to = forms.IntegerField(required=False, label=_('Send report when finishes (chat ID)'))
+    text = forms.CharField(
+        max_length=4000,
+        min_length=5,
+        widget=forms.Textarea(attrs={'placeholder': '<b>Hello</b>'}),
+        label=_('Broadcast message'),
+    )
+    results_notify_to = forms.IntegerField(
+        required=False,
+        label=_('Send report when finishes (chat ID)'),
+        widget=forms.NumberInput(attrs={'placeholder': '283568193'}),
+    )
