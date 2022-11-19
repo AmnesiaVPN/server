cb8db880cbc47308a98fb58d7b7123bdf47c08d7
Eldos <eldos.baktybekov@gmail.com>
2022-11-19 17:54:24 +0600

    :fire: Remove unused code

--- donationalerts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from donationalerts.views import payment_confirm_view
-
-urlpatterns = [
-    path('<str:telegram_id>/payment/', payment_confirm_view),
-]
--- vobla_vpn/urls.py
+++ vobla_vpn/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('telegram_bot.urls')),
     path('users/', include('wireguard.urls')),
-    path('users/', include('donationalerts.urls')),
 ]
