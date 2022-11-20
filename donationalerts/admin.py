c0a6b493488745fb18819b3cbbeb467b32c769cb
Eldos <eldos.baktybekov@gmail.com>
2022-11-20 11:06:33 +0600

    :children_crossing: Add verbosity for Payment model

--- donationalerts/admin.py
+++ donationalerts/admin.py
@@ -5,4 +5,6 @@ from donationalerts.models import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    pass
+    ordering = ('-created_at',)
+    list_filter = ('is_used',)
+    readonly_fields = ('donation_id', 'created_at',)
--- donationalerts/models.py
+++ donationalerts/models.py
@@ -7,4 +7,7 @@ class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     donation_id = models.BigIntegerField(unique=True, db_index=True)
     created_at = models.DateTimeField()
-    is_used = models.BooleanField(default=False)
+    is_used = models.BooleanField(default=False, verbose_name='Was payment used for subscription renewal')
+
+    def __str__(self):
+        return f'Payment #{self.donation_id}'
