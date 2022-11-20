dbdac16cf579830da8a12ab59bca31100118b605
Eldos <eldos.baktybekov@gmail.com>
2022-11-20 11:06:04 +0600

    :bug: Fix filtering existing ids

--- donationalerts/tasks.py
+++ donationalerts/tasks.py
@@ -1,5 +1,6 @@
 import datetime
 import itertools
+import logging
 
 from celery import shared_task
 from django.conf import settings
@@ -19,10 +20,11 @@ from wireguard.services import vpn_server
 def sync_payments_in_donationalerts_and_server():
     client = DonationAlertsClient(settings.DONATION_ALERTS_ACCESS_TOKEN)
     donations_from_donationalerts = tuple(itertools.chain.from_iterable(client.iter_all_donations()))
-    donation_ids_in_database = set(Payment.objects.values_list('donation_id'))
+    donation_ids_in_database = set(Payment.objects.values_list('donation_id', flat=True))
     donations_not_in_database = [donation for donation in donations_from_donationalerts
                                  if donation.id not in donation_ids_in_database]
 
+    logging.debug(f'Donation IDs in database {donation_ids_in_database}')
     users = User.objects.values('id', 'telegram_id')
 
     donations_to_insert_to_database = []
@@ -31,6 +33,7 @@ def sync_payments_in_donationalerts_and_server():
         for user in users:
             if str(user['telegram_id']) not in donation.message.lower():
                 continue
+            logging.info(f'New payment to user {user["telegram_id"]}')
             payment = Payment(user_id=user['id'], created_at=donation.created_at, donation_id=donation.id)
             donations_to_insert_to_database.append(payment)
             user_telegram_ids_to_be_notified.append(user['telegram_id'])
@@ -43,7 +46,7 @@ def sync_payments_in_donationalerts_and_server():
         try:
             telegram.send_message(telegram_id, text)
         except TelegramAPIError:
-            print(f'Could not send message to user {telegram_id}')
+            logging.warning(f'Could not send message to user {telegram_id}')
 
 
 @shared_task
