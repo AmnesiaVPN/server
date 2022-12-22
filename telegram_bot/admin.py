26eab23ad19d659a92c897705f6f5d6d71467d6d
Eldos <eldos.baktybekov@gmail.com>
2022-12-22 20:35:36 +0600

    :children_crossing: Add search by user telegram ID

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -6,3 +6,5 @@ from telegram_bot.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('registered_at',)
+    search_fields = ('telegram_id',)
+    search_help_text = 'User Telegram ID'
