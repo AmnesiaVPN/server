d1fbd44f15fe28afb2b8c748e2c2f8bc1bfe8fe4
Eldos <eldos.baktybekov@gmail.com>
2022-12-22 20:26:04 +0600

    :children_crossing: Add search user's donations

--- donationalerts/admin.py
+++ donationalerts/admin.py
@@ -7,5 +7,6 @@ from donationalerts.models import Payment
 class PaymentAdmin(admin.ModelAdmin):
     ordering = ('-created_at',)
     list_filter = ('is_used',)
-    list_display = ('donation_id', 'is_used',)
-    readonly_fields = ('donation_id', 'created_at',)
+    list_display = ('donation_id', 'is_used', 'created_at')
+    search_fields = ('user__telegram_id',)
+    search_help_text = 'User Telegram ID'
