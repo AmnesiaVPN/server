7da7ab136219dbe08a178fa2dd06e7b4adbf2cb1
Eldos <eldos.baktybekov@gmail.com>
2023-01-11 00:10:10 +0600

    :technologist: Add min value validator for promocode activate view

--- promocodes/serializers.py
+++ promocodes/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class PromocodeSerializer(serializers.Serializer):
-    promocode = serializers.CharField(max_length=8)
+    promocode = serializers.CharField(max_length=8, min_length=8)
