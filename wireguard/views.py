ab7aeb4aad71808fcd769e0faf9a95c0d9cb098a
Eldos <eldos.baktybekov@gmail.com>
2023-01-11 19:28:03 +0600

    :construction: Turn view to api view

--- wireguard/views.py
+++ wireguard/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 from telegram_bot.selectors import get_user_or_raise_404
 from wireguard.services.vpn_server import VPNServerService
 
 
+@api_view(['GET'])
 def user_config_detail_api(request, telegram_id: int):
     user = get_user_or_raise_404(telegram_id=telegram_id, include_server=True)
     with VPNServerService(base_url=user.server.url, password=user.server.password) as vpn_server:
