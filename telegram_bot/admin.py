dd09f59c0c61ea04d28f1a251e6d666b8c6d1da2
Eldos <eldos.baktybekov@gmail.com>
2023-01-11 15:26:10 +0600

    :sparkles: Sync user creation in admin and vpn server

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from telegram_bot.models import User
+from wireguard.services import vpn_server
 
 
 @admin.register(User)
@@ -8,3 +9,13 @@ class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('registered_at',)
     search_fields = ('telegram_id',)
     search_help_text = 'User Telegram ID'
+
+    def save_model(self, request, obj, form, change):
+        server = obj.server
+        with vpn_server.connected_client(server.url, server.password) as client:
+            user = vpn_server.get_or_create_user(client, str(obj.telegram_id))
+        obj.uuid = user.uuid
+        super().save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
