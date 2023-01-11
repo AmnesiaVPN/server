3998f1349081bd3104196f41fd835cca224377c8
Eldos <eldos.baktybekov@gmail.com>
2023-01-11 20:52:57 +0600

    :card_file_box: Add optional name field in server model

--- wireguard/models.py
+++ wireguard/models.py
@@ -1,10 +1,17 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Server(models.Model):
-    url = models.CharField(unique=True, max_length=32)
+    name = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    url = models.URLField(unique=True, max_length=32)
     password = models.CharField(max_length=255)
-    max_users_count = models.PositiveSmallIntegerField()
+    max_users_count = models.PositiveSmallIntegerField(
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(100),
+        ),
+    )
 
     def __str__(self):
-        return self.url
+        return self.name or self.url
