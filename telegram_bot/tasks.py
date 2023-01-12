ce1571d9283cb7f42652486a569914e18bec6836
Eldos <eldos.baktybekov@gmail.com>
2023-01-12 23:54:52 +0600

    :recycle: Refactor task

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -2,23 +2,25 @@ import logging
 import time
 
 from celery import shared_task
+from django.conf import settings
 
-from telegram_bot.services import telegram
 from telegram_bot.exceptions import TelegramAPIError
 from telegram_bot.models import User
+from telegram_bot.services.telegram import TelegramMessagingService
 
 
 @shared_task
 def start_broadcasting(text: str, results_notify_to: int | None = None):
+    telegram_messaging_service = TelegramMessagingService(settings.BOT_TOKEN)
     user_telegram_ids = User.objects.values_list('telegram_id', flat=True)
     users_count = len(user_telegram_ids)
 
     sent_messages_count = 0
-    for user_telegram_id in user_telegram_ids:
+    for telegram_id in user_telegram_ids:
         try:
-            telegram.send_message(user_telegram_id, text)
+            telegram_messaging_service.send_message(chat_id=telegram_id, text=text)
         except TelegramAPIError:
-            logging.warning(f'Could not send message to {user_telegram_id}')
+            logging.warning(f'Could not send message')
         else:
             sent_messages_count += 1
         finally:
@@ -26,4 +28,4 @@ def start_broadcasting(text: str, results_notify_to: int | None = None):
 
     if results_notify_to is not None:
         report = f'{sent_messages_count}/{users_count} пользователей получили рассылку'
-        telegram.send_message(results_notify_to, report)
+        telegram_messaging_service.send_message(chat_id=results_notify_to, text=report)
