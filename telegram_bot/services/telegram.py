b225bf97b9c4aa45a6aefa0a7c7bb8b2ef6b8569
Eldos <eldos.baktybekov@gmail.com>
2023-01-13 21:55:14 +0600

    :building_construction: Use `TelegramMessage` structure

--- telegram_bot/services/telegram.py
+++ telegram_bot/services/telegram.py
@@ -7,8 +7,73 @@ from telegram_bot.exceptions import TelegramAPIError
 
 __all__ = (
     'TelegramMessagingService',
-    'get_payment_page_markup',
+    'SubscriptionExpiredMessage',
+    'SubscriptionExpiresInHoursMessage',
+    'TrialPeriodExpiredMessage',
 )
+
+PAYMENT_PAGE_MARKUP = {
+    'inline_keyboard': [
+        [
+            {
+                'text': 'Продлить подписку',
+                'url': settings.PAYMENT_PAGE_URL,
+            },
+        ],
+    ],
+}
+
+
+class TelegramMessage:
+    text: str | None = None
+    reply_markup: dict | None = None
+
+    def get_text(self) -> str:
+        return self.text
+
+    def get_reply_markup(self) -> dict:
+        return self.reply_markup
+
+
+class TrialPeriodExpiredMessage(TelegramMessage):
+    reply_markup = PAYMENT_PAGE_MARKUP
+
+    def __init__(self, *, telegram_id: int):
+        self.__telegram_id = telegram_id
+
+    def get_text(self) -> str:
+        return (
+            'Пробный период использования закончился. Вы отключены от VPN. Стоимость продления 299 рублей'
+            '\nВажно❗️'
+            f'\nПри оплате в комментарии укажите имя вашего файла <b>{self.__telegram_id}</b>'
+        )
+
+
+class SubscriptionExpiredMessage(TelegramMessage):
+    reply_markup = PAYMENT_PAGE_MARKUP
+
+    def __init__(self, *, telegram_id: int):
+        self.__telegram_id = telegram_id
+
+    def get_text(self) -> str:
+        return (
+            'Ваша подписка закончилась. Вы отключены от VPN. Стоимость продления 299 рублей'
+            '\nВажно❗️'
+            f'\nПри оплате в комментарии укажите имя вашего файла <b>{self.__telegram_id}</b>'
+        )
+
+
+class SubscriptionExpiresInHoursMessage(TelegramMessage):
+
+    def __init__(self, *, telegram_id: int, hours_before_expiration: int):
+        self.__telegram_id = telegram_id
+        self.__hours_before_expiration = hours_before_expiration
+
+    def get_text(self) -> str:
+        return (
+            f'Ваша подписка заканчивается через {self.__hours_before_expiration} час(ов)'
+            f'\nВажно❗️\nПри оплате в комментарии укажите имя вашего файла <b>{self.__telegram_id}</b>'
+        )
 
 
 class TelegramMessagingService:
@@ -17,9 +82,10 @@ class TelegramMessagingService:
     def __init__(self, token: str):
         self.__api_base_url = f'https://api.telegram.org/bot{token}'
 
-    def send_message(self, *, chat_id: int, text: str, reply_markup: Collection[Collection[dict]] | None = None):
+    def send_message(self, *, chat_id: int, message: TelegramMessage):
         url = f'{self.__api_base_url}/sendMessage'
-        request_body = {'chat_id': chat_id, 'text': text, 'parse_mode': 'html'}
+        request_body = {'chat_id': chat_id, 'text': message.get_text(), 'parse_mode': 'html'}
+        reply_markup = message.get_reply_markup()
         if reply_markup is not None:
             request_body['reply_markup'] = reply_markup
         try:
@@ -28,7 +94,3 @@ class TelegramMessagingService:
             raise TelegramAPIError
         if not response.is_success:
             raise TelegramAPIError
-
-
-def get_payment_page_markup() -> dict[str, list[list[dict]]]:
-    return {'inline_keyboard': [[{'text': 'Продлить подписку', 'url': settings.PAYMENT_PAGE_URL}]]}
