8f4ad7845c253e273e954bea2dc9c23ffe58cd0f
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 15:02:53 +0600

    :card_file_box: Add `ScheduledTask` and `has_activated_promocode` field

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -11,6 +14,25 @@ class User(models.Model):
     is_trial_period = models.BooleanField(default=True)
     registered_at = models.DateTimeField(auto_now_add=True)
     subscribed_at = models.DateTimeField(default=timezone.now)
+    has_activated_promocode = models.BooleanField(default=False)
 
     def __str__(self):
         return f'User: {self.telegram_id}'
+
+    @property
+    def subscription_days_count(self) -> int:
+        return settings.TRIAL_PERIOD_DAYS if self.is_trial_period else settings.SUBSCRIPTION_DAYS
+
+    @property
+    def is_subscribed(self) -> bool:
+        now = timezone.now()
+        return now < self.subscription_expires_at
+
+    @property
+    def subscription_expires_at(self) -> datetime.datetime:
+        return self.subscribed_at + datetime.timedelta(days=self.subscription_days_count)
+
+
+class ScheduledTask(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
