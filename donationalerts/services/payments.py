0784bdba9d9c15646bd429082820be838df4cb16
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 10:44:28 +0600

    :sparkles: Add services to change user subscription status

--- donationalerts/services/payments.py
+++ donationalerts/services/payments.py
@@ -1,7 +1,13 @@
 from typing import Iterable
 
+from django.db import transaction
+from django.utils import timezone
+
 from donationalerts.models import Payment
 from donationalerts.schemas import ValidatedDonationalertsPayment
+from telegram_bot.models import User
+from wireguard.models import Server
+from wireguard.services.vpn_server import VPNServerService
 
 
 def batch_create_payments(
@@ -17,3 +23,23 @@ def batch_create_payments(
         ) for validated_payment in validated_payments
     ]
     Payment.objects.bulk_create(payments)
+
+
+@transaction.atomic
+def activate_subscription(*, user: User, server: Server, payment: Payment):
+    payment.is_used = True
+    user.subscribed_at = timezone.now()
+    user.is_trial_period = False
+    payment.save()
+    user.save()
+    with VPNServerService(base_url=server.url, password=server.password) as vpn_server:
+        vpn_server.login()
+        vpn_server.enable_user(user.uuid)
+
+
+def deactivate_subscription(*, user: User, server: Server):
+    user.is_trial_period = False
+    user.save()
+    with VPNServerService(base_url=server.url, password=server.password) as vpn_server:
+        vpn_server.login()
+        vpn_server.disable_user(user.uuid)
