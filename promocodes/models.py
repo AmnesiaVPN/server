f1ff51499a19806eef691fc9f34ed7264fc9bada
Eldos <eldos.baktybekov@gmail.com>
2023-01-14 15:05:19 +0600

    :card_file_box: Models changes
    * Change FK to O2O field `activated_by` in `Promocode` model.
    * Remove `count` field in `PromocodesGroup`.
    * Add `total_count` property.

--- promocodes/models.py
+++ promocodes/models.py
@@ -1,4 +1,3 @@
-from django.core.validators import MinValueValidator
 from django.db import models
 
 from telegram_bot.models import User
@@ -6,19 +5,22 @@ from telegram_bot.models import User
 
 class PromocodesGroup(models.Model):
     name = models.CharField(max_length=64)
-    count = models.PositiveIntegerField(validators=(MinValueValidator(1),))
     expire_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+    @property
+    def total_count(self):
+        return self.promocode_set.count()
+
 
 class Promocode(models.Model):
     value = models.CharField(max_length=8, unique=True)
     group = models.ForeignKey(PromocodesGroup, on_delete=models.CASCADE)
     activated_at = models.DateTimeField(null=True, blank=True)
-    activated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    activated_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.value
