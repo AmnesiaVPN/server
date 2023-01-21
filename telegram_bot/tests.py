817e2172a57310cb7a5f79aac37a17f3641a2a9a
Eldos <eldos.baktybekov@gmail.com>
2023-01-21 12:15:05 +0600

    :white_check_mark: VOB-3 Add tests

--- telegram_bot/tests.py
+++ telegram_bot/tests.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 
 from telegram_bot.models import User
 from wireguard.models import Server
-from telegram_bot.selectors import get_subscription_expired_users
+from donationalerts.models import Payment
+from telegram_bot.selectors import get_subscription_expired_users, get_subscription_expired_users_with_payments
 
 
 class SubscriptionExpiredUsersSelectorTestCase(TestCase):
@@ -15,11 +16,11 @@ class SubscriptionExpiredUsersSelectorTestCase(TestCase):
     def setUp(self) -> None:
         now = timezone.now()
         server = Server.objects.create(url='http://localhost:5000', password='1234', max_users_count=100)
-        User.objects.create(
+        self.user_1 = User.objects.create(
             telegram_id=1, uuid=uuid.uuid4(), server=server, is_trial_period=False,
             subscribed_at=now - datetime.timedelta(days=settings.SUBSCRIPTION_DAYS, minutes=1),
         )
-        User.objects.create(
+        self.user_2 = User.objects.create(
             telegram_id=2, uuid=uuid.uuid4(), server=server, is_trial_period=False,
             subscribed_at=now - datetime.timedelta(days=settings.SUBSCRIPTION_DAYS, minutes=-1),
         )
@@ -39,6 +40,9 @@ class SubscriptionExpiredUsersSelectorTestCase(TestCase):
             telegram_id=6, uuid=uuid.uuid4(), server=server, is_trial_period=False,
             subscribed_at=now - datetime.timedelta(days=settings.SUBSCRIPTION_DAYS, hours=-20),
         )
+        Payment.objects.create(user=self.user_1, donation_id=12345, created_at=now)
+        Payment.objects.create(user=self.user_2, donation_id=1234, created_at=now)
+        self.donation_ids = {12345, 1234}
         self.expired_ids = {1, 3, 5}
         self.not_expired_ids = {2, 4, 6}
 
@@ -51,3 +55,7 @@ class SubscriptionExpiredUsersSelectorTestCase(TestCase):
         users = get_subscription_expired_users().values_list('telegram_id', flat=True)
         for expired_id in self.not_expired_ids:
             self.assertNotIn(expired_id, users)
+
+    def test_get_users_with_expired_subscription_and_with_payments(self):
+        users = get_subscription_expired_users_with_payments()
+        self.assertIn(self.user_1, users)
