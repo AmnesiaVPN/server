6e44da437f15b13e39b5a9c70230d26a9803bc26
Eldos <eldos.baktybekov@gmail.com>
2023-01-23 22:48:27 +0600

    :building_construction: VOB-8 Change subscription control way

--- wireguard/tasks.py
+++ wireguard/tasks.py
@@ -1,13 +1,14 @@
-import datetime
 import logging
 
 from celery import shared_task
 from django.conf import settings
+from django.utils import timezone
 
 from donationalerts.exceptions import UserHasNoUnusedPaymentError
 from donationalerts.selectors import get_oldest_unused_payment
-from telegram_bot.selectors import get_user, get_subscription_expired_users_with_payments
-from telegram_bot.services.scheduled_tasks import remove_previously_scheduled_tasks, create_new_scheduled_tasks
+from telegram_bot.exceptions import TelegramAPIError
+from telegram_bot.models import User
+from telegram_bot.selectors import get_user, get_subscribed_users, get_subscription_expired_users_with_payments
 from telegram_bot.services.telegram import TelegramMessagingService, SubscriptionExpiresInHoursMessage
 from telegram_bot.services.users import on_subscription_activated, on_subscription_deactivated
 
@@ -19,8 +20,41 @@ def subscription_control_task():
     for user in users:
         payment = user.payment_set.first()
         on_subscription_activated(telegram_messaging_service=telegram_messaging_service, user=user, payment=payment)
-        on_user_subscription_date_updated.delay(telegram_id=user.telegram_id)
         logging.info(f'User {user.telegram_id} subscription was activated')
+
+
+def check_subscription(telegram_messaging_service: TelegramMessagingService, user: User):
+    try:
+        payment = get_oldest_unused_payment(user=user)
+    except UserHasNoUnusedPaymentError:
+        on_subscription_deactivated(telegram_messaging_service=telegram_messaging_service, user=user)
+    else:
+        on_subscription_activated(telegram_messaging_service=telegram_messaging_service, user=user, payment=payment)
+
+
+@shared_task
+def subscription_expiration_control_task():
+    telegram_messaging_service = TelegramMessagingService(settings.BOT_TOKEN)
+    now = timezone.now()
+    users = get_subscribed_users()
+    for user in users:
+        seconds_before_expiration = (user.subscription_expires_at - now).total_seconds()
+        logging.debug(f'User ID{user.telegram_id} {seconds_before_expiration} seconds before expiration')
+        hours_before_expiration = int(seconds_before_expiration // 3600)
+        is_last_minute_of_hour = seconds_before_expiration % 3600 <= 60
+        if hours_before_expiration == 0 and is_last_minute_of_hour:
+            check_subscription(telegram_messaging_service, user)
+            continue
+        if hours_before_expiration not in (1, 24) or not is_last_minute_of_hour:
+            continue
+        message = SubscriptionExpiresInHoursMessage(
+            telegram_id=user.telegram_id,
+            hours_before_expiration=hours_before_expiration,
+        )
+        try:
+            telegram_messaging_service.send_message(chat_id=user.telegram_id, message=message)
+        except TelegramAPIError:
+            logging.error(f'Could not send message to user {user.telegram_id}')
 
 
 @shared_task
