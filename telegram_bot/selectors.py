88c6893397b4c17e8b96f25135328d674400c2c6
Eldos <eldos.baktybekov@gmail.com>
2023-01-23 22:19:57 +0600

    :recycle: VOB-8 Refactor user selectors
    * Add method to get subscribed_users.

--- telegram_bot/selectors.py
+++ telegram_bot/selectors.py
@@ -5,7 +5,7 @@ from django.db.models import QuerySet, F, Q
 from django.db.models.functions import Now
 
 from telegram_bot.exceptions import UserNotFoundError
-from telegram_bot.models import User, ScheduledTask
+from telegram_bot.models import User
 from telegram_bot.schemas import UserIDAndTelegramID
 
 __all__ = (
@@ -13,6 +13,7 @@ __all__ = (
     'get_all_user_ids_and_telegram_ids',
     'get_subscription_expired_users',
     'get_subscription_expired_users_with_payments',
+    'get_subscribed_users',
 )
 
 
@@ -30,22 +31,47 @@ def get_all_user_ids_and_telegram_ids() -> QuerySet[UserIDAndTelegramID]:
     return User.objects.values('id', 'telegram_id')
 
 
-def get_previously_scheduled_tasks(*, user: User | int) -> QuerySet[ScheduledTask]:
-    return ScheduledTask.objects.filter(user=user)
+def filter_subscribed_users(users: QuerySet[User]) -> QuerySet[User]:
+    return users.filter(
+        Q(user_subscription_expires_at__gt=Now(), is_trial_period=False)
+        | Q(user_trial_period_expires_at__gt=Now(), is_trial_period=True)
+    )
+
+
+def get_users(*, include_server: bool = False) -> QuerySet[User]:
+    users = User.objects.all()
+    if include_server:
+        users = users.select_related('server')
+    return users
 
 
 def get_subscription_expired_users() -> QuerySet[User]:
-    return (
-        User.objects
-        .select_related('server')
-        .annotate(
-            user_subscription_expires_at=F('subscribed_at') + datetime.timedelta(days=settings.SUBSCRIPTION_DAYS),
-            user_trial_period_expires_at=F('subscribed_at') + datetime.timedelta(days=settings.TRIAL_PERIOD_DAYS),
-        )
-        .filter(
-            Q(user_subscription_expires_at__lte=Now(), is_trial_period=False)
-            | Q(user_trial_period_expires_at__lte=Now(), is_trial_period=True)
-        )
+    return filter_subscription_expired_users(
+        annotate_subscription_expiration_date(
+            get_users(include_server=True),
+        ),
+    )
+
+
+def get_subscribed_users() -> QuerySet[User]:
+    return filter_subscribed_users(
+        annotate_subscription_expiration_date(
+            get_users(include_server=True),
+        ),
+    )
+
+
+def filter_subscription_expired_users(users: QuerySet[User]) -> QuerySet[User]:
+    return users.filter(
+        Q(user_subscription_expires_at__lte=Now(), is_trial_period=False)
+        | Q(user_trial_period_expires_at__lte=Now(), is_trial_period=True)
+    )
+
+
+def annotate_subscription_expiration_date(users: QuerySet[User]) -> QuerySet[User]:
+    return users.annotate(
+        user_subscription_expires_at=F('subscribed_at') + datetime.timedelta(days=settings.SUBSCRIPTION_DAYS),
+        user_trial_period_expires_at=F('subscribed_at') + datetime.timedelta(days=settings.TRIAL_PERIOD_DAYS),
     )
 
 
